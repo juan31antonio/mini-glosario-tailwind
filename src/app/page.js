dfd4d2a79a01b1535bd1839cb8c265f982f7ef07
juan31antonio <juanantoniocorder32@gmail.com>
2025-02-03 12:54:27 +0100

    tailwind boletin

--- src/app/page.js
+++ src/app/page.js
@@ -2,100 +2,59 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+   <div>
+      <Card informacion={{
+          titulo:"bg-green-300, m-4, p-3",
+          descripcion: `Estas tres clases son usadas en todos los cards de este glosario.
+          - El bg-green-200 es usado para el color de fondo de los div.
+          - El m-4 es usado para dar un margen con un tamaño de 4, tamaño que calcula automáticamente Tailwind.
+          - El p-3 es usado para dar un padding con un tamaño de 3, tamaño que calcula automáticamente Tailwind.`,
+          clase: "",
+          informacionClase:"El ejemplo visual es el propio card."}}
+          ></Card>
+      <Card 
+        informacion={{
+          titulo:"text-*, text-*, font-*",
+          descripcion: `Estas tres clases son usadas para editar el estilo del texto.
+                        - El text-* (text-color-valor) se usa con algun color y un valor para el tono del color y aplica este color a la letra.
+                        - El text-* (text-tamaño) se usa con un tamaño definido por tailwind y aplica ese tamaño a la letra.
+                        - El font-* (font-fuente, font-estilo) es usado para aplicarle una fuente a la letra o tambien estilo, como negrita.   `,
+          clase:"text-red-400 text-2xl font-sans font-bold.",
+          informacionClase:`
+                      Ejemplo de color (text-red-400)
+                      Ejemplo de tamaño (text-2xl)
+                      Ejemplo de fuente (font-sans)
+                      Ejemplo de estilo (font-bold)`
+        }}
+      ></Card>
+      <Card
+      informacion={{
+        titulo:"rounded-* shadow-*",
+        descripcion:`Estas dos clases las usamos para un estilo mas visual.
+      - El rounded-* (rounded-tamaño) se usa con los tamaños predefinidos de Tailwind para redondear bordes.
+      - El shadow-* (shadow-tamaño) se usa para aplicar una sombra exterior usando los tamaños predefinidos de Tailwind.
+        `,
+        clase:"bg-blue-300 rounded-lg shadow-xl",
+        informacionClase:"Esto es el ejemplo donde redondeamos el pre y le aplicamos una sombra"
+      }}></Card>
+      <Card
+      informacion={{
+        titulo:"flex, justify-*, items-*",
+        descripcion:``,
+        clase: "",
+        informacionClase:""
+      }}
+      ></Card>
+   </div>
+  );
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+function Card({informacion}){
+  return(
+    <div className="bg-green-200 m-4 p-3 rounded-lg max-w-5xl">
+      <p className="text-2xl font-bold">{informacion.titulo}</p><br/>
+      <p className="break-words text-xl whitespace-pre-line">{informacion.descripcion}</p><br/>
+      <p className={`mb-1 break-words whitespace-pre-line ${informacion.clase}`}>{informacion.informacionClase}</p>
     </div>
   );
 }
